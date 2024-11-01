07a7c3dce6d2cb1b6e0eff205a955b9ceab764f7
sdjhs0327 <sdjhs0327@naver.com>
2024-11-01 16:37:04 +0900

    commit

--- script/modules/APTdataloadaer.py
+++ script/modules/APTdataloadaer.py
@@ -5,9 +5,19 @@ import requests
 import xmltodict
 import math
 
+sigungu_cd = {
+            '종로구': '11110', '중구': '11140', '용산구': '11170', '성동구': '11200', '광진구': '11215',
+            '동대문구': '11230', '중랑구': '11260', '성북구': '11290', '강북구': '11305', '도봉구': '11320',
+            '노원구': '11350', '은평구': '11380', '서대문구': '11410', '마포구': '11440', '양천구': '11470',
+            '강서구': '11500', '구로구': '11530', '금천구': '11545', '영등포구': '11560', '동작구': '11590',
+            '관악구': '11620', '서초구': '11650', '강남구': '11680', '송파구': '11710', '강동구': '11740'
+            }
+
+authKey = 'G7GuVfz99A0umUDOAuZlRXMYlZmKxBd3hHsj8EiZ89SSD7r5qrn08q70LM96fyblxiM3eGxWsgkIjhF%2BAWTVBg%3D%3D'
+
 class AptTradingDataloader(object):
-    def __init__(self):
-        self.authKey = 'G7GuVfz99A0umUDOAuZlRXMYlZmKxBd3hHsj8EiZ89SSD7r5qrn08q70LM96fyblxiM3eGxWsgkIjhF%2BAWTVBg%3D%3D'
+    def __init__(self, authKey):
+        self.authKey = authKey
         self.raw_cols = {"aptDong": "단지동명", "aptNm": "단지명", "aptSeq": "단지일련번호", "bonbun": "법정동본번", "bubun": "법정동부번",
                      "buildYear": "건축년도", "buyerGbn": "매수자유형명", "cdealDay": "해제사유발생일자", "cdealType": "해제여부",
                      "dealAmount": "거래금액", "dealDay": "거래일자", "dealMonth": "거래월", "dealYear": "거래년도", "dealingGbn": "거래구분명",
@@ -27,13 +37,7 @@ class AptTradingDataloader(object):
                      '단지일련번호', '단지명', '전용면적제곱미터', '층수', '건축년도', ## 무엇을
                      '거래구분명', '거래금액', ## 어떻게
                      '해제여부'] ## 기타
-        self.sigungu_cd = {
-            '종로구': '11110', '중구': '11140', '용산구': '11170', '성동구': '11200', '광진구': '11215',
-            '동대문구': '11230', '중랑구': '11260', '성북구': '11290', '강북구': '11305', '도봉구': '11320',
-            '노원구': '11350', '은평구': '11380', '서대문구': '11410', '마포구': '11440', '양천구': '11470',
-            '강서구': '11500', '구로구': '11530', '금천구': '11545', '영등포구': '11560', '동작구': '11590',
-            '관악구': '11620', '서초구': '11650', '강남구': '11680', '송파구': '11710', '강동구': '11740'
-            }
+
 
     ## APT 매매이력 API
     def get_apt_trading_raw(self, sigungu_cd, base_month):
@@ -66,3 +70,46 @@ class AptTradingDataloader(object):
         dataset = dataset[data_cols]
         df = dataset[cols]
         return dataset, df
+
+
+class AptRentDataloader(object):
+    def __init__(self, authKey):
+        self.authKey = authKey
+        self.raw_cols = {"aptNm":'단지명', 'buildYear':'건축년도', 'contractTerm':'계약기간', 'contractType':'거래구분명', 'dealDay':'거래일자',
+                        'dealMonth':'거래월', 'dealYear':'거래년도', 'deposit':'보증금액', 'excluUseAr':'전용면적제곱미터', 'floor':'층수',
+                        'jibun':'법정동지번내용', 'monthlyRent':'월세금액', 'preDeposit':'종전계약보증금', 'preMonthlyRent':'종전계약월세',
+                        'sggCd':'법정동시군구코드', 'umdNm':'법정동읍면동명', 'useRRRight':'갱신요구권사용여부'}
+
+        self.data_cols = ['거래년도', '거래월', '거래일자', ## 언제
+                        '법정동시군구코드', '법정동읍면동명', '법정동지번내용', ## 어디서
+                        '단지명', '전용면적제곱미터', '층수', '건축년도', ## 무엇을
+                        '거래구분명', '보증금액', '월세금액', '종전계약보증금', '종전계약월세', '갱신요구권사용여부'] ## 어떻게
+            
+    def get_apt_rent_raw(self, sigungu_cd, base_month):
+        authKey = self.authKey
+        try:
+            num_of_rows = 100
+            url = f"http://apis.data.go.kr/1613000/RTMSDataSvcAptRent/getRTMSDataSvcAptRent?serviceKey={authKey}&LAWD_CD={sigungu_cd}&DEAL_YMD={base_month}"
+            response = requests.get(url)
+            raw = xmltodict.parse(response.text)
+            total_cnt = int(raw['response']['body']['totalCount'])
+            max_page = int(math.ceil(total_cnt//num_of_rows))
+            raw = raw['response']['body']['items']['item']
+            dataset = pd.DataFrame(raw)
+            if max_page > 1:
+                for page_no in range(2, max_page+1):
+                    url = f"http://apis.data.go.kr/1613000/RTMSDataSvcAptTradeDev/getRTMSDataSvcAptTradeDev?serviceKey={authKey}&LAWD_CD={sigungu_cd}&DEAL_YMD={base_month}&pageNo={page_no}&numOfRows={num_of_rows}"
+                    response = requests.get(url)
+                    raw = xmltodict.parse(response.text)
+                    raw = raw['response']['body']['items']['item']
+                    dataset = pd.concat([dataset, pd.DataFrame(raw)])
+            return dataset
+        except Exception as e:
+                return print(f'{e} {base_month}, {sigungu_cd} 결과를 가져오는데 실패했습니다.')
+            
+    def preproc_apt_trading(self, apt_rant_raw):
+        raw_cols = self.raw_cols
+        data_cols = self.data_cols
+        dataset = apt_rant_raw.rename(columns=raw_cols)
+        dataset = dataset[data_cols]
+        return dataset
